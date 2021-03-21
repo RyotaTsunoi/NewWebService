feb091b7c89638a67ca1b91e344605a79d3c4af8
RyotaTsunoi <rytoa0056@gmail.com>
2021-03-22 00:32:43 +0900

    ログインサンプルの実装

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,17 +1,19 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import React from 'react';
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-    ß
-  </Layout>
-);
+import Layout from '../components/layout';
+import { useFetchUser } from '../lib/user';
 
-export default AboutPage;
+const About: React.FC = () => {
+  const { user, loading } = useFetchUser();
+
+  return (
+    <Layout user={user} loading={loading}>
+      <h1>About</h1>
+      <p>
+        This is the about page, navigating between this page and <i>Home</i> is always pretty fast. However, when you
+        navigate to the <i>Profile</i> page it takes more time because it uses SSR to fetch the user first;
+      </p>
+    </Layout>
+  );
+};
+export default About;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,34 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+import Layout from '../components/layout';
+import { useFetchUser } from '../lib/user';
 
-export default IndexPage
+export default function Home(): React.ReactElement {
+  const { user, loading } = useFetchUser();
+
+  return (
+    <Layout user={user} loading={loading}>
+      <h1>Next.js and Auth0 Example</h1>
+
+      {loading && <p>Loading login info...</p>}
+
+      {!loading && !user && (
+        <>
+          <p>
+            To test the login click in <i>Login</i>
+          </p>
+          <p>
+            Once you have logged in you should be able to click in <i>Profile</i> and <i>Logout</i>
+          </p>
+        </>
+      )}
+
+      {user && (
+        <>
+          <h4>Rendered user info on the client</h4>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </>
+      )}
+    </Layout>
+  );
+}
